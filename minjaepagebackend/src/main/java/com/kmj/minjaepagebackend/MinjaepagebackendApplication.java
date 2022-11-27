05b9b051aa5616dcd8ae12ad7efb3f3f0b2e4c8c
KIM MINJAE <minjae2717@gmail.com>
2022-11-27 21:33:40 +0900

    [update]Vue와 Spring Boot연동
    SpringBoot에서 GetMapping으로 /api/items 로 items를 호출하였으나
    WebSocket이슈가 계속 발생

---
+++ minjaepagebackend/src/main/java/com/kmj/minjaepagebackend/MinjaepagebackendApplication.java
@@ -0,0 +1,13 @@
+package com.kmj.minjaepagebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinjaepagebackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinjaepagebackendApplication.class, args);
+	}
+
+}
---
+++ minjaepagebackend/src/test/java/com/kmj/minjaepagebackend/MinjaepagebackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kmj.minjaepagebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinjaepagebackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
